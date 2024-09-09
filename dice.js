41cfb32540c18e465e694c506aad78be9f8ee616
Pikin <pikin@bk.ru>
2024-09-09 16:45:23 +0500

    first commit

---
+++ dice.js
@@ -0,0 +1,8 @@
+const playingDice = function () {
+    const numberOfDices = parseInt(prompt("Please enter the number of dice."));
+    const numberOfFaces = prompt("Please enter the number of faces on the dice");
+
+    for (let i = 1; i <= numberOfDices; i++) {
+        
+    }
+}
