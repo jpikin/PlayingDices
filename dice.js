2d12c538cfcd99b68238fc87e07190d39d5e0546
Pikin <pikin@bk.ru>
2024-09-09 16:56:48 +0500

    for added

--- dice.js
+++ dice.js
@@ -1,8 +1,10 @@
 const playingDice = function () {
     const numberOfDices = parseInt(prompt("Please enter the number of dice."));
-    const numberOfFaces = prompt("Please enter the number of faces on the dice");
-
+    const numberOfFaces = parseInt(prompt("Please enter the number of faces on the dice"));
+    const dices = []
     for (let i = 1; i <= numberOfDices; i++) {
-        
+        dices.push(Math.floor(Math.random() * numberOfFaces) + 1);
     }
+    return dices;
 }
+console.log(playingDice());
